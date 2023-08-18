38efd44a7892cd68a72032edd76d376d8f8697c7
Maria T. Andersen <mteilmann93@gmail.com>
2023-08-18 16:27:00 +0200

    Implement checking for horizontal sequence (partially)

--- src/App.jsx
+++ src/App.jsx
@@ -18,23 +18,39 @@ function App() {
                       className="cell"
                       key={cellNumber}
                       onClick={(event) => {
-                        const newBoard = [...board];
-                        if (newBoard[rowNumber][cellNumber].token !== null)
-                          return;
+                        // if the cell is already taken, nothing happens
+                        if (board[rowNumber][cellNumber].token !== null) return;
+                        if (board[rowNumber][cellNumber].face === "X") return;
 
-                        event.currentTarget.classList.add(
-                          window.players[turnNumber % 3]
-                        );
-                        newBoard[rowNumber][cellNumber].token =
-                          window.players[turnNumber % 3];
+                        // copy board
+                        const newBoard = [...board];
+                        // get current player
+                        const playerToken = window.players[0];
+
+                        event.currentTarget.classList.add(playerToken);
+
+                        newBoard[rowNumber][cellNumber].token = playerToken;
+
+                        // console.log(`(${rowNumber}, ${cellNumber})`);
                         setBoard(newBoard);
-                        // const winner = checkWinningConditions(newboard);
+
+                        const winner = checkWinningConditions(
+                          newBoard,
+                          playerToken,
+                          { rowNumber, cellNumber }
+                        );
+
+                        if (winner) {
+                          alert(`${playerToken} won!`);
+                        }
 
                         setTurnNumber(turnNumber + 1);
                       }}
                     >
                       <div className="face">{cell.face}</div>
-                      <div className="token">{cell.token}</div>
+                      {cell.face === "X" ? null : (
+                        <div className="token">{cell.token}</div>
+                      )}
                     </td>
                   );
                 })}
@@ -46,5 +62,58 @@ function App() {
     </div>
   );
 }
+
+function checkWinningConditions(board, token, position) {
+  console.log("====================================================");
+  // put the current player's token into corners
+  board[0][0].token = token;
+  board[0][9].token = token;
+  board[9][0].token = token;
+  board[9][9].token = token;
+
+  if (checkHorizontals(board, token, position)) return true;
+  if (checkVerticals(board, token, position)) return true;
+  // if (checkDiagonals(board, token, position)) return true;
+
+  return false;
+}
+
+function checkHorizontals(board, token, position) {
+  let sequence = false;
+
+  // check horizontals
+  for (let j = 0; j <= 5; j++) {
+    let countInARow = 0;
+    let breakOut = false;
+
+    /*eslint for-direction: "off"*/
+    for (let i = position.cellNumber + j; i >= position.cellNumber - 5; i--) {
+      console.log(`Checking (${position.rowNumber}, ${i}) (j == ${j})`);
+
+      if (token === board[position.rowNumber][i]?.token) {
+        console.log(`Found token at (${position.rowNumber}, ${i})`);
+        countInARow++;
+        if (countInARow === 5) {
+          sequence = true;
+          break;
+        }
+      }
+
+      if (token !== board[position.rowNumber][i]?.token) {
+        sequence = false;
+        if (i > position.cellNumber) breakOut = true;
+        break;
+      }
+    }
+    if (breakOut) break;
+    if (sequence) break;
+  }
+
+  return sequence;
+}
+
+function checkVerticals(board, token, position) {}
+
+// function checkDiagonals(board, token, position) {}
 
 export default App;
