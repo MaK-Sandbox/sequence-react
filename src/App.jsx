bbf4e737d495642b0d772e70f49f8700abe10606
Maria T. Andersen <mteilmann93@gmail.com>
2023-08-18 14:41:16 +0200

    Render board and enable placing tokens

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [board, setBoard] = useState(window.board);
+  const [turnNumber, setTurnNumber] = useState(0);
+
+  return (
+    <div className="board">
+      <table>
+        <tbody>
+          {board.map((row, rowNumber) => {
+            return (
+              <tr className="row" key={rowNumber}>
+                {row.map((cell, cellNumber) => {
+                  return (
+                    <td
+                      className="cell"
+                      key={cellNumber}
+                      onClick={(event) => {
+                        const newBoard = [...board];
+                        if (newBoard[rowNumber][cellNumber].token !== null)
+                          return;
+
+                        event.target.classList.add(
+                          window.players[turnNumber % 3]
+                        );
+                        newBoard[rowNumber][cellNumber].token =
+                          window.players[turnNumber % 3];
+                        setBoard(newBoard);
+
+                        setTurnNumber(turnNumber + 1);
+                      }}
+                    >
+                      <div className="face">{cell.face}</div>
+                      <div className="token">{cell.token}</div>
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
