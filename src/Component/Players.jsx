88c5e1d4b360aefe92fa4d4d2c49e2111a510f06
Maria T. Andersen <mteilmann93@gmail.com>
2023-08-24 14:04:20 +0200

    Insert tokens for each player and give the list of player a describing header

--- src/Component/Players.jsx
+++ src/Component/Players.jsx
@@ -4,16 +4,24 @@ import "./Players.css";
 
 function Players() {
   const [players, setPlayers] = useState([
-    { name: "Sam", token: "red", startedCurrentRound: false },
-    { name: "Eva", token: "blue", startedCurrentRound: false },
-    { name: "Niels", token: "green", startedCurrentRound: true },
+    { name: "Sam", token: window.players[0], startedCurrentRound: false },
+    { name: "Eva", token: window.players[1], startedCurrentRound: false },
+    { name: "Niels", token: window.players[2], startedCurrentRound: true },
   ]);
 
   return (
     <div className="Players">
+      <h2>Players</h2>
       {players.length > 0
         ? players.map((player, index) => {
-            return <Player key={index} player={player} />;
+            return (
+              <Player
+                key={index}
+                name={player.name}
+                token={player.token}
+                startedCurrentRound={player.startedCurrentRound}
+              />
+            );
           })
         : null}
     </div>
