c32407d47388d126ef5a5278b973a2c5b8253ff1
Maria T. Andersen <mteilmann93@gmail.com>
2023-08-24 14:58:56 +0200

    Add a title attribute so an explanation to the beginner symbol appears, when the user hovers over it

--- src/Component/Player.jsx
+++ src/Component/Player.jsx
@@ -5,7 +5,7 @@ function Player(props) {
   const { name, token, startedCurrentRound, isActivePlayer } = props;
   return (
     <div className={isActivePlayer ? "Player Active" : "Player"}>
-      <span className="Player_beginner_status">
+      <span className="Player_beginner_status" title="I started this round!">
         {startedCurrentRound ? "🔰" : null}
       </span>
       <span className="Player_name">{name}</span>
