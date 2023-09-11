66c5f1d2b6da1dcb631170b2fbbaf17cddeb340a
Maria T. Andersen <mteilmann93@gmail.com>
2023-09-11 20:56:45 +0200

    Call backend to create a match and render basic lobby

--- src/Component/Lobby.jsx
+++ src/Component/Lobby.jsx
@@ -1,5 +1,11 @@
 import style from "./Lobby.module.css";
 
-export default function Lobby() {
-  return <div className={style.Lobby}>{"I'm a lobby"}</div>;
+export default function Lobby({ players }) {
+  return (
+    <div className={style.Lobby}>
+      {players?.map((player, index) => (
+        <div key={index}>Player: {player?.name}</div>
+      ))}
+    </div>
+  );
 }
--- src/pages/CreateTable.jsx
+++ src/pages/CreateTable.jsx
@@ -1,8 +1,27 @@
 import { Link } from "react-router-dom";
 import "./CreateTable.css";
 import Lobby from "../Component/Lobby";
+import { useEffect, useState } from "react";
 
 export default function CreateTable() {
+  const [table, setTable] = useState({});
+
+  useEffect(() => {
+    createTable();
+
+    async function createTable() {
+      const response = await fetch(`${window.API_URL}/matches/create`, {
+        method: "POST",
+      });
+      if (!response.ok) {
+        // handle error
+        return;
+      }
+      const table = await response.json();
+      setTable(table);
+    }
+  }, []);
+
   return (
     <div className="create-table">
       <div className="flex">
@@ -12,7 +31,7 @@ export default function CreateTable() {
       </div>
 
       <div>
-        <Lobby />
+        <Lobby players={table?.players} />
       </div>
 
       <Link to="/">🡸 Go back to Main Menu</Link>
