cb2636154165a32b1a0fc479a70933b6ddc330f2
Maria T. Andersen <mteilmann93@gmail.com>
2023-09-11 07:43:33 +0200

    Create a button to return back to the main menu from the JoinTable component

--- src/pages/JoinTable.jsx
+++ src/pages/JoinTable.jsx
@@ -1,9 +1,13 @@
-// import "./Home.css";
+import { Link } from "react-router-dom";
+import "./JoinTable.css";
 
 export default function JoinTable() {
   return (
     <div className="join-table">
-      <div className="container">Join Table</div>
+      <div className="container">
+        <Link to="/">🡸 Go back to Main Menu</Link>
+        Join Table
+      </div>
     </div>
   );
 }
