e224ac5a8ed7dc7f6e5a41c3f812163dde2a3f79
Maria T. Andersen <mteilmann93@gmail.com>
2023-09-11 20:27:29 +0200

    Create simple CreateTable component layout

---
+++ src/Component/Lobby.jsx
@@ -0,0 +1,5 @@
+import style from "./Lobby.module.css";
+
+export default function Lobby() {
+  return <div className={style.Lobby}>{"I'm a lobby"}</div>;
+}
--- src/pages/CreateTable.jsx
+++ src/pages/CreateTable.jsx
@@ -1,11 +1,21 @@
 import { Link } from "react-router-dom";
 import "./CreateTable.css";
+import Lobby from "../Component/Lobby";
 
 export default function CreateTable() {
   return (
     <div className="create-table">
+      <div className="flex">
+        <button>Ready?</button>
+        <button>Start</button>
+        <button>Invite</button>
+      </div>
+
+      <div>
+        <Lobby />
+      </div>
+
       <Link to="/">🡸 Go back to Main Menu</Link>
-      Create Table
     </div>
   );
 }
