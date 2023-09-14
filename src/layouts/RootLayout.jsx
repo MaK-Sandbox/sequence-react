3e54e0712f24d15eb80deb4b1a68e56beaa86dce
Maria T. Andersen <mteilmann93@gmail.com>
2023-09-14 18:59:57 +0200

    Make wsTable single source of truth

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -6,6 +6,8 @@ export default function RootLayout() {
   const [isConnected, setIsConnected] = useState(false);
   const [socket] = useState(window.socket);
   const [wsMessage, setWsMessage] = useState("");
+  const [wsName, setWsName] = useState("");
+  const [wsTable, setWsTable] = useState({});
 
   useEffect(() => {
     function onConnect() {
@@ -21,20 +23,34 @@ export default function RootLayout() {
       setWsMessage(message);
     }
 
+    function onName(name) {
+      console.log(name);
+      setWsName(name);
+    }
+
+    function onTable(table) {
+      console.log(table);
+      setWsTable(table);
+    }
+
     socket.on("connect", onConnect);
     socket.on("disconnect", onDisconnect);
     socket.on("message", onMessage);
+    socket.on("username", onName);
+    socket.on("table", onTable);
 
     return () => {
       socket.off("connect", onConnect);
       socket.off("disconnect", onDisconnect);
       socket.off("message", onMessage);
+      socket.off("username", onName);
+      socket.off("table", onTable);
     };
   }, [socket]);
 
   return (
     <div className={style["rootLayout"]}>
-      <Outlet context={[wsMessage, socket]} />
+      <Outlet context={{ wsMessage, wsName, wsTable, setWsTable, socket }} />
     </div>
   );
 }
