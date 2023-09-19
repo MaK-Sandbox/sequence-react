e1a9e1ed2f4151ef5db1689166004c6f8cc82d7d
Maria T. Andersen <mteilmann93@gmail.com>
2023-09-19 11:55:39 +0200

    Create a separate component for changing name of the player

---
+++ src/Component/SetUsername.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { useOutletContext } from "react-router-dom";
+import { updateUsername } from "../communications";
+import style from "./SetUsername.module.css";
+
+export default function SetUsername() {
+  const [tempName, setTempName] = useState("");
+  const { username } = useOutletContext();
+
+  return (
+    <div className={style["set-username"]}>
+      {username === "" ? null : (
+        <span className="hello-player">{`Hello ${username}`}</span>
+      )}
+      <span className="set-name-container">
+        <h2>Choose a name</h2>
+        <input
+          type="text"
+          placeholder="name"
+          onChange={(event) => setTempName(event.target.value)}
+        />
+        <button
+          onClick={() => {
+            updateUsername(tempName);
+          }}
+        >
+          SAVE
+        </button>
+      </span>
+    </div>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,11 @@
-import { useState } from "react";
-import { Link, useOutletContext } from "react-router-dom";
-import { updateUsername } from "../communications";
+import { Link } from "react-router-dom";
 import "./Home.css";
+import SetUsername from "../Component/SetUsername";
 
 export default function Home() {
-  const [tempName, setTempName] = useState("");
-  const { username } = useOutletContext();
-
   return (
     <div className="home-page">
-      {username === "" ? null : (
-        <span className="hello-player">{`Hello ${username}`}</span>
-      )}
-      <span className="set-name-container">
-        <h2>Choose a name</h2>
-        <input
-          type="text"
-          placeholder="name"
-          onChange={(event) => setTempName(event.target.value)}
-        />
-        <button
-          onClick={() => {
-            updateUsername(tempName);
-          }}
-        >
-          SAVE
-        </button>
-      </span>
+      <SetUsername />
       <span className="create-table-container">
         <h2>Create new table</h2>
         <Link to="/matches/create">Create table</Link>
