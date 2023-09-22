6c0bd5494e908b8f6099b2f565b9b161c0136195
Maria T. Andersen <mteilmann93@gmail.com>
2023-09-22 16:36:01 +0200

    Clean up code

--- src/pages/Match.jsx
+++ src/pages/Match.jsx
@@ -1,53 +1,21 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import style from "./Match.module.css";
 import Players from "../Component/Players";
 import Board from "../Component/Board";
 import Piles from "../Component/Piles";
 import Hand from "../Component/Hand";
 import Scores from "../Component/Scores";
-import { io } from "socket.io-client";
 // import Player from "../Component/Player";
-
-const socket = io(window.API_URL);
 
 export default function App() {
   const [turnNumber, setTurnNumber] = useState(0);
   const [teams, setTeams] = useState(window.teams);
   const [players, setPlayers] = useState([...window.players]);
   const [drawPile, setDrawPile] = useState([...window.cards]);
-  // const [discardPile, setDiscardPile] = useState([]);
   const [playedCard, setPlayedCard] = useState("");
-  const [isConnected, setIsConnected] = useState(false);
-
-  const [wsMessage, setWsMessage] = useState("");
-
-  useEffect(() => {
-    function onConnect() {
-      setIsConnected(true);
-    }
-
-    function onDisconnect() {
-      setIsConnected(false);
-    }
-
-    function onMessage(message) {
-      setWsMessage(message);
-    }
-
-    socket.on("connect", onConnect);
-    socket.on("disconnect", onDisconnect);
-    socket.on("message", onMessage);
-
-    return () => {
-      socket.off("connect", onConnect);
-      socket.off("disconnect", onDisconnect);
-      socket.off("message", onMessage);
-    };
-  }, []);
 
   return (
     <div className={style["game-view"]}>
-      {wsMessage}
       <Players players={players} />
       <Scores teams={teams} />
       <Board
