38b98a80f1550d6b199f6e7d06c743e7fb4b0ae0
Maria T. Andersen <mteilmann93@gmail.com>
2023-09-29 21:36:55 +0200

    Enable players who join the game to edit their username

--- src/pages/JoinRoom.jsx
+++ src/pages/JoinRoom.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { Link, useOutletContext, useParams } from "react-router-dom";
 import { joinTable } from "../communications";
+import SetUsername from "../Component/SetUsername";
 import ReadyButton from "../Component/ReadyButton";
 import Lobby from "../Component/Lobby";
 import "./JoinRoom.css";
@@ -16,6 +17,7 @@ export default function JoinTable() {
 
   return (
     <div className="join-table">
+      <SetUsername />
       <ReadyButton />
       <Link to="/">🡸 Go back to Main Menu</Link>
       Match ID: {tableId}
