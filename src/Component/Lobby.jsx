027d8f6e505709dbe728af36bf3422973211cdb6
Maria T. Andersen <mteilmann93@gmail.com>
2023-10-04 17:22:30 +0200

    Generate a grid inside of Lobby which displays information on players who are currently connected to a match

--- src/Component/Lobby.jsx
+++ src/Component/Lobby.jsx
@@ -3,14 +3,21 @@ import style from "./Lobby.module.css";
 export default function Lobby({ teams }) {
   return (
     <div className={style["Lobby"]}>
-      <h2>Current players in the room</h2>
-      <ul>
+      <h2>Connected players</h2>
+      <div className={style["grid-container"]}>
+        <div className={style["grid-header"]}>Team</div>
+        <div className={style["grid-header"]}>Username</div>
+        <div className={style["grid-header"]}>Ready</div>
         {teams.map((team) =>
           team?.players.map((player, index) => (
-            <li key={index}>{player.username}</li>
+           <>
+            <div key={index}>{team.token}</div>
+            <div key={index}>{player.username}</div>
+            <div key={index}>{`${player.ready}`}</div>
+            </>
           ))
         )}
-      </ul>
+      </div>
     </div>
   );
 }
