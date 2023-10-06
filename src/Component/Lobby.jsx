ccdb0706fd68ee27c5787133b268aec41425e14f
Maria T. Andersen <mteilmann93@gmail.com>
2023-10-06 16:13:39 +0200

    Mark in current user in the lobby

--- src/Component/Lobby.jsx
+++ src/Component/Lobby.jsx
@@ -1,7 +1,10 @@
 import { useEffect } from "react";
 import style from "./Lobby.module.css";
+import { useOutletContext } from "react-router-dom";
 
 export default function Lobby({ teams, setIsEveryoneReady }) {
+  const { socket } = useOutletContext();
+
   useEffect(() => {
     let playerCount = 0;
     let readyCounts = 0;
@@ -38,15 +41,30 @@ export default function Lobby({ teams, setIsEveryoneReady }) {
         {teams.map((team) =>
           team.players.map((player, index) => (
             <>
-              <div key={index * 10 + 1} data-playerid={player.id}>
+              <div
+                key={index * 10 + 1}
+                data-playerid={player.id}
+                className={
+                  player.id === socket.id ? style["highlight-left-edge"] : null
+                }
+              >
                 {team.token}
               </div>
-              <div key={index * 10 + 2} data-playerid={player.id}>
-                {player.username}
+              <div
+                key={index * 10 + 2}
+                data-playerid={player.id}
+                className={player.id === socket.id ? style["highlight"] : null}
+              >
+                {player.id === socket.id
+                  ? `${player.username} (you)`
+                  : player.username}
               </div>
               <div
                 key={index * 10 + 3}
                 data-playerid={player.id}
+                className={
+                  player.id === socket.id ? style["highlight-right-edge"] : null
+                }
               >{`${player.ready}`}</div>
             </>
           ))
