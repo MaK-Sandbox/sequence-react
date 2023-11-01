72e286f38734e29191ed937230c3b4ee97b8d2f5
Maria T. Andersen <mteilmann93@gmail.com>
2023-11-01 20:06:30 +0100

    Adjust the style of score chart according to the number of teams playing

--- src/Component/Scores.jsx
+++ src/Component/Scores.jsx
@@ -1,15 +1,24 @@
+import { useState, useEffect } from "react";
 import style from "./Scores.module.css";
 
 export default function Scores({ teams }) {
+  const [teamCount, setTeamCount] = useState(2);
+
+  useEffect(() => {
+    setTeamCount(teams.length);
+  }, [teams]);
+
   return (
     <div className={style["Scores"]}>
       <h2>Score</h2>
-      <div className={style["Current-scores"]}>
+      <div
+        className={teamCount > 2 ? style["Three-teams"] : style["Two-teams"]}
+      >
         {Object.keys(teams).map((key, index) => {
           return (
             <div className={style["Score"]} key={index}>
-              <p>{key}</p>
-              <p>{teams[key].sequenceCount}</p>
+              {/* <p>{key}</p> */}
+              <p>{teams[key].sequences.length}</p>
             </div>
           );
         })}
