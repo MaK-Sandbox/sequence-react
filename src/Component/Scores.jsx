e8677cd647c91f082f947d830cbe56ff1b4c3479
Maria T. Andersen <mteilmann93@gmail.com>
2023-11-06 14:06:32 +0100

    Display team score in the Scores component

--- src/Component/Scores.jsx
+++ src/Component/Scores.jsx
@@ -1,28 +1,25 @@
-import { useState, useEffect } from "react";
 import style from "./Scores.module.css";
 
 export default function Scores({ teams }) {
-  const [teamCount, setTeamCount] = useState(2);
-
-  useEffect(() => {
-    setTeamCount(teams.length);
-  }, [teams]);
-
   return (
     <div className={style["Scores"]}>
       <h2>Score</h2>
-      <div
-        className={teamCount > 2 ? style["Three-teams"] : style["Two-teams"]}
-      >
-        {Object.keys(teams).map((key, index) => {
-          return (
-            <div className={style["Score"]} key={index}>
-              {/* <p>{key}</p> */}
-              <p>{teams[key].sequences.length}</p>
-            </div>
-          );
-        })}
-      </div>
+      <span className={style["test"]}>
+        <div className={style["Scores-container"]}>
+          {teams && Array.isArray(teams)
+            ? teams.map((team, i) => {
+                if (typeof team === "object" && team !== null) {
+                  return (
+                    <div key={i * 10} className={style["Score"]}>
+                      <div key={i * 10 + 1}>{team.token}</div>
+                      <div key={i * 10 + 2}>{team.sequences.length}</div>
+                    </div>
+                  );
+                }
+              })
+            : null}
+        </div>
+      </span>
     </div>
   );
 }
