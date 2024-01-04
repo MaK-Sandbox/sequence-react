fb46e39c07fa9914454daaee81c20c3ce9e8b10b
Maria T. Andersen <mteilmann93@gmail.com>
2024-01-05 00:30:02 +0100

    Style the discard pile similarly to the draw pile

--- src/Component/DiscardPile.jsx
+++ src/Component/DiscardPile.jsx
@@ -1,3 +1,11 @@
+import style from "./DiscardPile.module.css";
+
 export default function DiscardPile() {
-  return <div>DISCARDED</div>;
+  return (
+    <div className={style.discardPile}>
+      <div className={style.cell}>
+        <div className={style.face}>♠️Q</div>
+      </div>
+    </div>
+  );
 }
