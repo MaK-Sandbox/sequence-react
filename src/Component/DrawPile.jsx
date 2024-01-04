0607d7a38d6fff3a315d3f4f712c634a34ca05e2
Maria T. Andersen <mteilmann93@gmail.com>
2024-01-05 00:04:53 +0100

    The drawpile should show the backside of the cards instead of a face

--- src/Component/DrawPile.jsx
+++ src/Component/DrawPile.jsx
@@ -5,7 +5,11 @@ export default function DrawPile() {
   return (
     <div className={style.drawPile}>
       <div className={style.cell} onClick={() => drawCard()}>
-        <div className={style.face}>♠️Q</div>
+        <img
+          className={style.backsideOfCard}
+          src="https://i.pinimg.com/originals/7c/4d/01/7c4d015c0684e8da7f1da10fc29038c5.jpg"
+          alt="backside of card"
+        />
       </div>
     </div>
   );
