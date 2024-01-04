a5fb75c82d19c29d31fc7e46ac6605919bc50a8f
Maria T. Andersen <mteilmann93@gmail.com>
2024-01-05 00:39:34 +0100

    Simplify the component

--- src/Component/DrawPile.jsx
+++ src/Component/DrawPile.jsx
@@ -4,17 +4,13 @@ import style from "./DrawPile.module.css";
 export default function DrawPile() {
   return (
     <div className={style.drawPile}>
-      <div
+      <img
         className={style.cell}
+        src="https://i.pinimg.com/originals/7c/4d/01/7c4d015c0684e8da7f1da10fc29038c5.jpg"
+        alt="backside of card"
         onClick={() => drawCard()}
         title="Draw a card"
-      >
-        <img
-          className={style.backsideOfCard}
-          src="https://i.pinimg.com/originals/7c/4d/01/7c4d015c0684e8da7f1da10fc29038c5.jpg"
-          alt="backside of card"
-        />
-      </div>
+      />
     </div>
   );
 }
