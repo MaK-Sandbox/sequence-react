e71b9ef1aa336224f0fde2d59db6bcac2f4c9566
Maria T. Andersen <mteilmann93@gmail.com>
2024-01-06 01:26:50 +0100

    Display the card drawn from the draw pile visually in the hand component

--- src/Component/Hand.jsx
+++ src/Component/Hand.jsx
@@ -1,25 +1,18 @@
-import { useEffect } from "react";
 import { useOutletContext } from "react-router-dom";
 import style from "./Hand.module.css";
 
-export default function Hand({ cards, setPlayedCard }) {
+export default function Hand() {
   const { card } = useOutletContext();
-
-  useEffect(() => {
-    console.log("card: ", card);
-  }, [card]);
 
   return (
     <div className={style["Hand"]}>
-      {cards.map((card, index) => (
-        <span
-          className={style["Card"]}
-          key={index}
-          onClick={() => setPlayedCard(card)}
-        >
-          {card}
-        </span>
-      ))}
+      <span className={style["Card"]}>{card}</span>
+      <span className={style["Card"]}>{card}</span>
+      <span className={style["Card"]}>{card}</span>
+      <span className={style["Card"]}>{card}</span>
+      <span className={style["Card"]}>{card}</span>
+      <span className={style["Card"]}>{card}</span>
+      <span className={style["Card"]}>{card}</span>
     </div>
   );
 }
--- src/pages/Match.jsx
+++ src/pages/Match.jsx
@@ -2,10 +2,9 @@ import style from "./Match.module.css";
 import Players from "../Component/Players";
 import Board from "../Component/Board";
 import Piles from "../Component/Piles";
-// import Hand from "../Component/Hand";
+import Hand from "../Component/Hand";
 import Scores from "../Component/Scores";
 import { useOutletContext } from "react-router-dom";
-// import Player from "../Component/Player";
 
 export default function Match() {
   const { wsTable: match } = useOutletContext();
@@ -16,10 +15,7 @@ export default function Match() {
       <Scores teams={match.teams} />
       <Board board={match.board} />
       <Piles />
-      {/* <Hand
-        cards={match.players.find((p) => p.isYou).cards}
-        setPlayedCard={setPlayedCard}
-      /> */}
+      <Hand />
     </div>
   );
 }
