5df8aec4ce7541834c43d061d9f4fe05d1e2ca9a
Maria T. Andersen <mteilmann93@gmail.com>
2024-01-16 17:43:44 +0100

    Create a new component to represent all card components that make the board

--- src/Component/Board.jsx
+++ src/Component/Board.jsx
@@ -1,32 +1,7 @@
-import { useRef } from "react";
+import BoardCards from "./BoardCards";
 import style from "./Board.module.css";
 
 export default function Board({ board, selected }) {
-  const cardRef = useRef("");
-
-  function isHighlighted(selectedCard, cell) {
-    /**
-     * Black jacks mean that the current player can place a token on any free spot on the board.
-     * We do not want cells with the face of "X" to be highlighted.
-     */
-    if (["♣️J", "♠️J"].includes(selectedCard) && cell.face !== "X") {
-      return !cell.token;
-    }
-
-    /**
-     * Red jacks mean that the current player can remove any token on the board that is not a part of a requence.
-     * We do not want cells with the face of "X" to be highlighted.
-     */
-    if (["♦️J", "♥️J"].includes(selectedCard) && cell.face !== "X") {
-      return cell.sequences.length === 0 && cell.token;
-    }
-
-    /**
-     * Highlight any cell which face matches the selected card, as long as the cell is not already occupied by a token.
-     */
-    return selectedCard === cell.face && !cell.token;
-  }
-
   return (
     <div className={style["Board"]}>
       <table>
@@ -36,29 +11,13 @@ export default function Board({ board, selected }) {
               <tr className={style["Row"]} key={rowNumber}>
                 {row.map((cell, cellNumber) => {
                   return (
-                    <td
-                      className={`${style["Cell"]} ${
-                        isHighlighted(selected, cell)
-                          ? style["highlighted"]
-                          : ""
-                      }`}
-                      data-row={rowNumber}
-                      data-cell={cellNumber}
+                    <BoardCards
                       key={cellNumber}
-                      ref={cardRef}
-                      onClick={(event) => {
-                        selected === cell.face
-                          ? console.log("Element: ", event.target)
-                          : null;
-                      }}
-                    >
-                      <div className="face" data-face={cell.face}>
-                        {cell.face}
-                      </div>
-                      {cell.face === "X" ? null : (
-                        <div className="token">{cell.token}</div>
-                      )}
-                    </td>
+                      cell={cell}
+                      cellNumber={cellNumber}
+                      rowNumber={rowNumber}
+                      selected={selected}
+                    />
                   );
                 })}
               </tr>
---
+++ src/Component/BoardCards.jsx
@@ -0,0 +1,49 @@
+import { useRef } from "react";
+import style from "./BoardCards.module.css";
+
+export default function BoardCards({ children, cell, cellNumber, rowNumber, selected }) {
+  const cardRef = useRef("");
+
+  function isHighlighted(selectedCard, cell) {
+    /**
+     * Black jacks mean that the current player can place a token on any free spot on the board.
+     * We do not want cells with the face of "X" to be highlighted.
+     */
+    if (["♣️J", "♠️J"].includes(selectedCard) && cell.face !== "X") {
+      return !cell.token;
+    }
+
+    /**
+     * Red jacks mean that the current player can remove any token on the board that is not a part of a requence.
+     * We do not want cells with the face of "X" to be highlighted.
+     */
+    if (["♦️J", "♥️J"].includes(selectedCard) && cell.face !== "X") {
+      return cell.sequences.length === 0 && cell.token;
+    }
+
+    /**
+     * Highlight any cell which face matches the selected card, as long as the cell is not already occupied by a token.
+     */
+    return selectedCard === cell.face && !cell.token;
+  }
+
+  return (
+    <td
+      className={`${style["Cell"]} ${
+        isHighlighted(selected, cell) ? style["highlighted"] : ""
+      }`}
+      data-row={rowNumber}
+      data-cell={cellNumber}
+      key={cellNumber}
+      ref={cardRef}
+      onClick={(event) => {
+        selected === cell.face ? console.log("Element: ", event.target) : null;
+      }}
+    >
+      <div className="face" data-face={cell.face}>
+        {cell.face}
+      </div>
+      {cell.face === "X" ? null : <div className="token">{cell.token}</div>}
+    </td>
+  );
+}
